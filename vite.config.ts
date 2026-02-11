7ab0ab630db68866b080a9655d4b288c66470961
Lokesh-Surya <91713027+Lokesh-Surya@users.noreply.github.com>
2026-02-12 02:02:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/cultural-weave-invite/",
   server: {
     host: "::",
     port: 8080,
